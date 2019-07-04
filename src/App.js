6eb374e443cbe1466f703dd7490c6064694e4833
Shubham Kushwaha <Shubham.kushwaha.star@gmail.com>
2019-07-04 18:43:27 +0530

    the api call is made using axios in the app.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from'axios';
+import { async } from 'q';
 
-const printTheResultForApp =(msg)=>{
+const printTheResultForApp = async (msg)=>{
     console.log(msg);
+
+    const response = await axios.get('https://api.unsplash.com/search/photos',
+    {
+        params:{query:msg},
+        headers: {
+            Authorization: 'Client-ID 564337fe7636c24113ab9363c9a2b4a7404dad2dba4cc31f4fcd828d1678dc67'
+        }
+    });
+
+    console.log(response)
 }
 
 const App = ()=>{
