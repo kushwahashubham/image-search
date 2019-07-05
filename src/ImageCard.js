2b867b9c2c0383e4cf5329f1ab95cc325dafc685
Shubham Kushwaha <Shubham.kushwaha.star@gmail.com>
2019-07-05 15:37:46 +0530

    The project is completed

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,12 +1,31 @@
 import React,{Component} from 'react';
 
 class ImageCard extends Component{
+    constructor(){
+        super();
+
+        this.imageRef = React.createRef();
+        this.state = {noOfSpans: 0};
+    }
+
+    correctRowHeights=()=>{
+        console.log(this.imageRef.current.clientHeight);
+
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height/10);
+        this.setState({noOfSpans: spans});
+    }
+
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load',this.correctRowHeights);
+    }
 
     render(){
         const {urls,alt_description} = this.props.image;
         return(
-            <div>
+            <div style={{gridRowEnd: `span ${this.state.noOfSpans}`}}>
                 <img
+                    ref={this.imageRef}
                     src={urls.regular}
                     alt={alt_description}
                 />
